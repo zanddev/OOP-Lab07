081b14b9dbac7bf743a0547891fa90b94206d844
Davide Zandonella <davide.zandonella@studio.unibo.it>
2021-11-09 16:34:15 +0100

    added sports to enum it.unibo.oop.lab.enum01.Sport

--- src/it/unibo/oop/lab/enum1/Sport.java
+++ src/it/unibo/oop/lab/enum1/Sport.java
@@ -6,12 +6,8 @@ package it.unibo.oop.lab.enum1;
 /**
  * Represents an enumeration for declaring sports;
  * 
- * 1) Complete the definition of the enumeration.
- * 
  */
 public enum Sport {
-    /*
-     * declare the following sports: - basket - soccer - tennis - bike - F1 -
-     * motogp - volley
-     */
+
+	basket, soccer, tennis, bike, F1, motogp, volley;
 }
