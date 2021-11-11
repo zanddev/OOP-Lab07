ff1c22e4f5d03bcac49bc7dbad9b93b7e56cf2d9
Davide Zandonella <davide.zandonella@studio.unibo.it>
2021-11-11 21:21:35 +0100

    complete enum2.Sports: defined methods

--- src/it/unibo/oop/lab/enum2/Sport.java
+++ src/it/unibo/oop/lab/enum2/Sport.java
@@ -6,14 +6,6 @@ package it.unibo.oop.lab.enum2;
 /**
  * Represents an enumeration for declaring sports.
  * 
- * 1) You must add a field keeping track of the number of members each team is
- * composed of (1 for individual sports)
- * 
- * 2) A second field will keep track of the name of the sport.
- * 
- * 3) A third field, of type Place, will allow to define if the sport is
- * practiced indoor or outdoor
- * 
  */
 public enum Sport {
 
@@ -29,7 +21,12 @@ public enum Sport {
 	SOCCER(Place.OUTDOOR, 11, "soccer");
 
 	/*
-	 * Declare required fields
+	 * Declare required fields:
+	 *  1. Field noTeamMembers is keeping track of the number of members
+	 *       each team is composed of (1 for individual sports)
+	 *  2. Field actualName that keeps track of the name of the sport.
+	 *  3. Field place, of type Place, will allow to define if the sport is
+	 *       practised indoor or outdoor
 	 */
 	private final int noTeamMembers;
 	private final String actualName;
@@ -45,29 +42,37 @@ public enum Sport {
 		this.actualName = actualName;
 	}
 
-    /*
-     * TODO
-     * 
-     * [METHODS] To be defined
-     * 
-     * 
-     * 1) public boolean isIndividualSport()
-     * 
-     * Must return true only if called on individual sports
-     * 
-     * 
-     * 2) public boolean isIndoorSport()
-     * 
-     * Must return true in case the sport is practices indoor
-     * 
-     * 
-     * 3) public Place getPlace()
-     * 
-     * Must return the place where this sport is practiced
-     * 
-     * 
-     * 4) public String toString()
-     * 
-     * Returns the string representation of a sport
-     */
+	/**
+	 * Checks if the sport is practised individually
+	 * 
+	 * @return true only if called on individual sports
+	 */
+	public boolean isIndividualSport() {
+		return (this.noTeamMembers == 1);
+	}
+
+	/**
+	 * 
+	 * @return true in case the sport is practices indoor
+	 */
+	public boolean isIndoorSport() {
+		return (this.place == Place.INDOOR);
+	}
+
+	/**
+	 * 
+	 * @return the place where this sport is practised
+	 */
+	public Place getPlace() {
+		return this.place;
+	}
+
+	/**
+	 * Returns the name of this enum constant, as contained in the declaration
+	 * 
+	 * @return the string representation of a sport
+	 */
+	public String toString() {
+		return this.actualName;
+	}
 }
