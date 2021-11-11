7b8eac28fcad7ccb803d4fb21918e7fc9b94fcb8
Davide Zandonella <davide.zandonella@studio.unibo.it>
2021-11-11 20:55:12 +0100

    create enumeration enum2.Place

---
+++ src/it/unibo/oop/lab/enum2/Place.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package it.unibo.oop.lab.enum2;
+
+/**
+ * Represents an enumeration for defeining if a sport is
+ * practiced indoor or outdoor
+ *
+ */
+public enum Place {
+
+	INDOOR, OUTDOOR;
+}
