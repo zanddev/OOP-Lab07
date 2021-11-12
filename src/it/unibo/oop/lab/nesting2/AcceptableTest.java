fb655acc042f167f3b1415abbd4a99621b55d685
Davide Zandonella <davide.zandonella@studio.unibo.it>
2021-11-12 16:56:25 +0100

    create in nesting2.AcceptableTest the OneListAcceptable objects instead of null values

--- src/it/unibo/oop/lab/nesting2/AcceptableTest.java
+++ src/it/unibo/oop/lab/nesting2/AcceptableTest.java
@@ -8,7 +8,8 @@ import java.util.List;
 
 import org.junit.Test;
 
-import it.unibo.oop.lab.nesting2.Acceptor.ElementNotAcceptedException;
+//import it.unibo.oop.lab.nesting2.Acceptor.ElementNotAcceptedException;
+//import it.unibo.oop.lab.nesting2.Acceptor.EndNotAcceptedException;
 
 /**
  * This is the JUnit test for {@link it.unibo.oop.lab.nesting2.Acceptable}
@@ -26,10 +27,7 @@ public class AcceptableTest {
 		 * Correct acceptance of [10, 20, 30, 40]
 		 */
 		final List<Integer> list = Arrays.asList(10, 20, 30, 40);
-		/*
-		 * the following needs instantiation
-		 */
-		final Acceptable<Integer> acc = null;
+		final Acceptable<Integer> acc = new OneListAcceptable<Integer>(list);
 		final Acceptor<Integer> acceptor = acc.acceptor();
 		for (final Integer el: list) {
 			acceptor.accept(el);
@@ -47,7 +45,7 @@ public class AcceptableTest {
 		 * Failing acceptance
 		 */
 		final List<Integer> list = Arrays.asList(10, 20, 30, 40);
-		final Acceptable<Integer> acc = null;
+		final Acceptable<Integer> acc = new OneListAcceptable<Integer>(list);
 		final Acceptor<Integer> acceptor = acc.acceptor();
 		try {
 			for (final Integer el: list) {
@@ -79,7 +77,7 @@ public class AcceptableTest {
 		 * Exception due to early end
 		 */
 		final List<Integer> list = Arrays.asList(10, 20, 30, 40);
-		final Acceptable<Integer> acc = null;
+		final Acceptable<Integer> acc = new OneListAcceptable<Integer>(list);
 		final Acceptor<Integer> acceptor = acc.acceptor();
 		try {
 			acceptor.accept(10);
