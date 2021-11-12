0571d4f977939ac7ba316fc440b2865191e5d776
Davide Zandonella <davide.zandonella@studio.unibo.it>
2021-11-12 02:20:22 +0100

    indent with tabs it.unibo.oop.lab.nesting2.* classes

--- src/it/unibo/oop/lab/nesting2/Acceptable.java
+++ src/it/unibo/oop/lab/nesting2/Acceptable.java
@@ -8,12 +8,11 @@ package it.unibo.oop.lab.nesting2;
  */
 public interface Acceptable<T> {
 
-    /**
-     * Return the acceptor, i.e. the object which will take a sequence of
-     * elements in the very same order as defined on acceptable.
-     * 
-     * @return the acceptor
-     */
-    Acceptor<T> acceptor();
-
+	/**
+	 * Return the acceptor, i.e. the object which will take a sequence of
+	 * elements in the very same order as defined on acceptable.
+	 * 
+	 * @return the acceptor
+	 */
+	Acceptor<T> acceptor();
 }
--- src/it/unibo/oop/lab/nesting2/Acceptor.java
+++ src/it/unibo/oop/lab/nesting2/Acceptor.java
@@ -9,59 +9,59 @@ package it.unibo.oop.lab.nesting2;
  */
 public interface Acceptor<T> {
 
-    /**
-     * Accept a new element. In case the element is not part of the set
-     * sequence, throws a {@link Acceptor.ElementNotAcceptedException}.
-     * 
-     * @param newElement
-     *            the next element to be accepted
-     * @throws ElementNotAcceptedException
-     *             if the element is out of the scheduled sequence
-     */
-    void accept(T newElement) throws ElementNotAcceptedException;
+	/**
+	 * Accept a new element. In case the element is not part of the set
+	 * sequence, throws a {@link Acceptor.ElementNotAcceptedException}.
+	 * 
+	 * @param newElement
+	 *            the next element to be accepted
+	 * @throws ElementNotAcceptedException
+	 *             if the element is out of the scheduled sequence
+	 */
+	void accept(T newElement) throws ElementNotAcceptedException;
 
-    /**
-     * Terminate to input new elements. If more elements have still to be
-     * inserted, throws an {@link Acceptor.EndNotAcceptedException}.
-     * 
-     * @throws EndNotAcceptedException
-     *             if more elements still need to be accepted
-     */
-    void end() throws EndNotAcceptedException;
+	/**
+	 * Terminate to input new elements. If more elements have still to be
+	 * inserted, throws an {@link Acceptor.EndNotAcceptedException}.
+	 * 
+	 * @throws EndNotAcceptedException
+	 *             if more elements still need to be accepted
+	 */
+	void end() throws EndNotAcceptedException;
 
-    /**
-     * Represents an exception occurring when trying to make the acceptor accept
-     * a wrong (out-of-sequence) element.
-     */
-    @SuppressWarnings("serial")
-    class ElementNotAcceptedException extends RuntimeException {
+	/**
+	 * Represents an exception occurring when trying to make the acceptor accept
+	 * a wrong (out-of-sequence) element.
+	 */
+	@SuppressWarnings("serial")
+	class ElementNotAcceptedException extends RuntimeException {
 
-        private final Object element;
+		private final Object element;
 
-        /**
-         * 
-         * @param element
-         *            the element that cause the exception to be raised
-         */
-        public ElementNotAcceptedException(final Object element) {
-            super();
-            this.element = element;
-        }
+		/**
+		 * 
+		 * @param element
+		 *            the element that cause the exception to be raised
+		 */
+		public ElementNotAcceptedException(final Object element) {
+			super();
+			this.element = element;
+		}
 
-        /**
-         * 
-         * @return the elements that caused this exception to be raised
-         */
-        public Object getElement() {
-            return element;
-        }
-    }
+		/**
+		 * 
+		 * @return the elements that caused this exception to be raised
+		 */
+		public Object getElement() {
+			return element;
+		}
+	}
 
-    /**
-     * Occurring when trying to terminate accepting new elements, but still
-     * other elements have to accepted.
-     */
-    @SuppressWarnings("serial")
-    class EndNotAcceptedException extends RuntimeException {
-    }
+	/**
+	 * Occurring when trying to terminate accepting new elements, but still
+	 * other elements have to accepted.
+	 */
+	@SuppressWarnings("serial")
+	class EndNotAcceptedException extends RuntimeException {
+	}
 }
